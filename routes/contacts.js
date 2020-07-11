6512856837f21d3f1becee51ca00562e62285b0b
Zlata <zlata@ellery.ru>
2020-07-11 22:49:52 +0300

    back ended

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,9 +12,9 @@ const Contact = require('../models/Contact');
 router.get('/', auth, async (req, res) => {
   try {
     const contacts = await Contact.find({ user: req.user.id })
-    .sort({date: -1});
+      .sort({ date: -1 });
     res.json(contacts)
-   
+
   } catch (err) {
     console.error(err);
     res.status(500).send('Server Error')
@@ -25,9 +25,38 @@ router.get('/', auth, async (req, res) => {
 // @route     POST api/contacts
 // @desc      Add new contact
 // @access    Private
-router.post('/', (req, res) => {
-  res.send('Add contact')
-})
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+      const contact = await newContact.save();
+      res.json(contact)
+    } catch (err) {
+      console.error(err)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 
 
 // @route     PUT api/contacts/:id
@@ -45,3 +74,5 @@ router.delete('/:id', (req, res) => {
 })
 
 module.exports = router;
+
+
