1ff0000e0b5aebda134f9dd7c63400714c861d86
Zlata <zlata@ellery.ru>
2020-07-11 22:04:24 +0300

    add to github

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route    GET api/contacts
 // @desc     Get all users contacts
 // @access   Private
-router.get('/', (req, res)=> {
-  res.send('Get all contacts')
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id })
+    .sort({date: -1});
+    res.json(contacts)
+   
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error')
+  }
 });
 
 
 // @route     POST api/contacts
 // @desc      Add new contact
 // @access    Private
-router.post('/', (req, res)=> {
+router.post('/', (req, res) => {
   res.send('Add contact')
 })
 
@@ -20,14 +33,14 @@ router.post('/', (req, res)=> {
 // @route     PUT api/contacts/:id
 // @desc      Update contact
 // @access    Private
-router.put('/:id', (req, res)=> {
+router.put('/:id', (req, res) => {
   res.send('Update contact')
 })
 
 // @route     DEELETE api/contacts/:id
 // @desc      Update contact
 // @access    Private
-router.delete('/:id', (req, res)=> {
+router.delete('/:id', (req, res) => {
   res.send('Delete contact')
 })
 
